d3c285ad925cec72aba47d0da1038f3ec858abc2
Damian <70946678+DamianvdD@users.noreply.github.com>
2020-09-18 15:14:04 +0200

    First Commit

---
+++ PoemPrinting.py
@@ -0,0 +1,7 @@
+Vers1 = "Ik ben Damian 16 en gamer vanaf hier wordt het lamer"
+Vers2 = "Ik woon in Zaandam en ben daar altijd lam"
+Vers3 = "Ik voetbal in Westaan daar zie je vaak staan"
+Vers4 = "Dit was mijn rijm met veel pijn"
+
+
+print(Vers1,Vers2,Vers3,Vers4)

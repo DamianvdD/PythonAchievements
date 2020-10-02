6a95ba159383f5770ad1df765d6d372e3bf8f97d
Damian <70946678+DamianvdD@users.noreply.github.com>
2020-10-02 14:38:12 +0200

    Eerste Versie

---
+++ PlayerStats.py
@@ -0,0 +1,39 @@
+# Alle stats van speler.
+
+Naam = "Damian Dragneel"
+JumpForce = 100
+SpecialPower = "Fire Dragon Slayer"
+Speed = "Faster then Light"
+Transport = "Happy the Cat"
+LivesLeft = 1
+PowerLevel = 100000
+Wanted = False
+PowerAttack = "Fire Dragon's Iron Fist"
+Ammo = "Eating fire to regen his powers"
+
+# Hier komen de lines.
+
+import time
+print("Dit zijn de Stats van", Naam)
+print("________________________")
+print(" ")
+print("Naam is:", Naam)
+time.sleep(1)
+print("Jumpforce is:", JumpForce)
+time.sleep(1)
+print("SpecialPower:", SpecialPower)
+time.sleep(1)
+print("Speed is:", Speed)
+time.sleep(1)
+print("Transport is:", Transport)
+time.sleep(1)
+print("Lives left is:", LivesLeft)
+time.sleep(1)
+print("Power Level is:", PowerLevel)
+time.sleep(1)
+print("Wanted:", Wanted)
+time.sleep(1)
+print("Power Attack is:", PowerAttack)
+time.sleep(1)
+print("Ammo is:", Ammo)
+print("________________________")

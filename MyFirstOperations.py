cc52beae853268825d9a65b26c6474fa32579b1c
Damian <70946678+DamianvdD@users.noreply.github.com>
2020-10-02 14:58:05 +0200

    Eerste Commit

---
+++ MyFirstOperations.py
@@ -0,0 +1,30 @@
+# De Variablen komen hier:
+
+Plus = 1+13
+Min = 13-1
+Keer = 6*4
+Delen = 65/3
+RestWaarde = 100%65
+Plus2 = 20
+Plus2 += 20
+Delen2 = 100
+Delen2 /= 4
+Min2 = 16
+Min2 -= 7
+Keer2 = 16
+Keer2 *= 32
+RestWaarde2 = 267
+RestWaarde2 /= 64
+
+# Hier komen de antwoorden:
+
+print(Plus)
+print(Min)
+print(Keer)
+print(Delen)
+print(RestWaarde)
+print(Plus2)
+print(Delen2)
+print(Min2)
+print(Keer2)
+print(RestWaarde2)
